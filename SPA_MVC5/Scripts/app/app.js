78db4503f6890b56a0b4439cdf41466204b44f9f
a.b.salikhov@gmail.com <a.b.salikhov@gmail.com>
2016-06-03 17:48:37 +0500

    first

---
+++ SPA_MVC5/Scripts/app/app.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="../angular.js" />
+var module = angular.module("StudentApp", ["ngRoute", "stucontrollers", "sturesources"]);
+
+module.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+        when('/home', {
+            templateUrl: 'templates/home.html',
+            controller: 'GetStudentsList'
+        }).
+        when('/addstudent', {
+            templateUrl: 'templates/home.html',
+            controller: 'GetStudentsList'
+        }).
+        when('/allstudents', {
+            templateUrl: 'templates/home.html',
+            controller: 'GetStudentsList'
+        }).
+        otherwise({
+            redirectTo: '/home'
+        });
+}])
